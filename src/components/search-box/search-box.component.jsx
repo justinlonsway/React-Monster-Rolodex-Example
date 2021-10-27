d5caaa2a336ac1ae49b5c4ddee917653dec4eb05
Justin Lonsway <cyberkurb@hotmail.com>
2021-10-27 07:54:43 -0700

    Add files via upload

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,10 @@
+import './search-box.style.css';
+
+export const SearchBox = ({ placeHolder, handleChange }) => (
+    <input 
+        className ='search'
+        type ='search' 
+        placeholder ={placeHolder} 
+        onChange ={handleChange} 
+        />
+)
